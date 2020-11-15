b4df4e820e7bc12204ee32862d1d8367f727c1c3
EanPistorius <ean.pistorius@gmail.com>
2020-11-15 12:50:28 +0200

    Revert "login page changes"
    This reverts commit 4dc74cf71c6241608c04436aa08b641feb2387d8.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    LoginComponent
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
