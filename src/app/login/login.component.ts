0f4c002044a654787f4120d63ac1394b75f860ab
EanPistorius <ean.pistorius@gmail.com>
2020-11-17 17:21:43 +0200

    Changes to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formGroup!: FormGroup;
+  constructor(private auth:AuthService) {}
+  ngOnInit(): void {}
+  initForm(){
+    this.formGroup = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
 
-  constructor() { }
-
-  ngOnInit(): void {
+    })
+  }
+  loginProcess(){
+    if(this.formGroup.valid){
+      this.auth.login(this.formGroup.value).subscribe(result => {
+        if(result.success){
+          console.log(result);
+          alert(result.success)
+        }else{
+          alert(result.message)
+        }
+      })
+    }
   }
 
 }
